90b7076b28d6df82d70f95cb8cf0d10087e5cd95
Ayesha <ayeshakhtar24@gmail.com>
2020-09-07 23:56:36 +0100

    challenge done 07/09/2020 incomplete

---
+++ countingSort2.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingSort function below.
+def countingSort(arr):
+    counter = [0 for i in range(100)]
+    for i in arr:
+        counter[i] += 1
+
+    f
+
+    return []
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
